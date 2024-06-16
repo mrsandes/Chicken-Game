0e2b35678ce8a9698ab55cad07afe1bbafedd37c
Marco Túlio Sandes Costa <mtsandes@yahoo.com>
2024-06-16 18:37:11 -0300

    camera controller

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,12 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour {
-    public Transform target;
-    private float smoothing = 0.01f;
-
+    public Transform target; 
+    public float smoothing; 
+    
+    public Vector2 minPosition;
+    public Vector2 maxPosition;
+    
     void LateUpdate() {
         if (transform.position != target.position) {
+
             Vector3 targetPosition = new Vector3(target.transform.position.x,target.transform.position.y, transform.position.z);
+
+            targetPosition.x = Mathf.Clamp(targetPosition.x, minPosition.x, maxPosition.x);
+            targetPosition.y = Mathf.Clamp(targetPosition.y, minPosition.y, maxPosition.y);
 
             transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
         }
